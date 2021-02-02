655e9a5766711a9509d023e31266921a24770d51
babrath <bert.abrath@UGent.be>
2021-02-02 21:39:59 +0100

    Adding -fno-builtin option to configure scripts. This to make sure **our** versions of strlen (and others) get used, not gcc's builtins.

--- configure-libc-partial-order-debug.sh
+++ configure-libc-partial-order-debug.sh
@@ -1,1 +1,3 @@
-CFLAGS="-Wno-maybe-uninitialized -fno-stack-protector -O1 -ggdb -fno-omit-frame-pointer -DMVEE_USE_TOTALPARTIAL_AGENT" ../configure --enable-stackguard-randomization --enable-obsolete-rpc --enable-pt_chown --with-selinux --enable-lock-elision=no --enable-addons=nptl --prefix=$HOME/glibc-build  --sysconfdir=/etc/
+# Use -fno-builtin to make sure our hooked versions of glibc functionality (like strlen) aren't replaced by gcc builtins
+# Use -Wno-alloc-larger-than to squash the warning that suddenly pops up when strlen is not a builtin on -O1 (what?)
+CFLAGS="-Wno-maybe-uninitialized -fno-stack-protector -O1 -ggdb -fno-omit-frame-pointer -fno-builtin -Wno-alloca-larger-than -DMVEE_USE_TOTALPARTIAL_AGENT" ../configure --enable-stackguard-randomization --enable-obsolete-rpc --enable-pt_chown --with-selinux --enable-lock-elision=no --enable-addons=nptl --prefix=$HOME/glibc-build  --sysconfdir=/etc/
--- configure-libc-woc.sh
+++ configure-libc-woc.sh
@@ -1,1 +1,2 @@
-../configure --enable-stackguard-randomization --enable-obsolete-rpc --enable-pt_chown --with-selinux --enable-lock-elision=no --enable-addons=nptl --prefix=$HOME/glibc-build --sysconfdir=$HOME/glibc-build/etc/
+# Use -fno-builtin to make sure our hooked versions of glibc functionality (like strlen) aren't replaced by gcc builtins
+CFLAGS="-O2 -fno-builtin" ../configure --enable-stackguard-randomization --enable-obsolete-rpc --enable-pt_chown --with-selinux --enable-lock-elision=no --enable-addons=nptl --prefix=$HOME/glibc-build --sysconfdir=$HOME/glibc-build/etc/
